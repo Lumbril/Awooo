e848e5cc1e68055eb78e0a40eda8788079fee2ed
Lumbril <32036622+Lumbril@users.noreply.github.com>
2022-09-28 18:31:06 +0300

    init commit

---
+++ Awooo/settings/prod.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent.parent.parent
+load_dotenv()
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
