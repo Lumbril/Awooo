ec46e4c1da9a3b7dc533c47177dd7dde5c3e9c52
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-08-10 15:27:19 +0300

    upd nginx conf and reformat ws routing

--- src/Awooo/asgi.py
+++ src/Awooo/asgi.py
@@ -13,24 +13,21 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django.urls import path
-
-from packs.middleware import JwtAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Awooo.settings')
 
 application = get_asgi_application()
 
-from api.websockets.chat_consumer import ChatConsumer
+from packs.middleware import JwtAuthMiddleware
+from routers.websocket_router import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": application,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             JwtAuthMiddleware(
-                URLRouter([
-                    path('ws/<int:user_id>', ChatConsumer.as_asgi())
-                ]))
+                URLRouter(websocket_urlpatterns)
+            )
         )
     ),
 })
--- src/routers/websocket_router.py
+++ src/routers/websocket_router.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.urls import path
 
 from api.websockets.chat_consumer import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<user_id>\w+)/$", ChatConsumer.as_asgi()),
+    path('ws/<int:user_id>', ChatConsumer.as_asgi()),
 ]
