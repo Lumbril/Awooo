1c48573f5055cefc6e5c41abee3eb4b1112ab470
Lumbril <32036622+Lumbril@users.noreply.github.com>
2023-08-10 16:39:50 +0300

    upd middleware

--- src/api/websockets/chat_consumer.py
+++ src/api/websockets/chat_consumer.py
@@ -1,6 +1,8 @@
 import json
+from json import JSONDecodeError
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import AnonymousUser
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -9,6 +11,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.user_id = None
 
     async def connect(self):
+        if isinstance(self.scope['user'], AnonymousUser):
+            return await self.close()
+
         self.user_id = f"userId_{self.scope['url_route']['kwargs']['user_id']}"
 
         await self.channel_layer.group_add(self.user_id, self.channel_name)
@@ -19,17 +24,34 @@ class ChatConsumer(AsyncWebsocketConsumer):
         pass
 
     async def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message = "text_data_json['message']"
+        try:
+            text_data_json = json.loads(text_data)
+            message = text_data_json['message']
 
-        await self.channel_layer.group_send(
-            self.user_id, {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+            await self.channel_layer.group_send(
+                self.user_id, {
+                    'type': 'chat_message',
+                    'message': message
+                }
+            )
+        except JSONDecodeError:
+            await self.channel_layer.group_send(
+                self.user_id, {
+                    'type': 'error',
+                    'error': 'Error in body, body must contain json'
+                }
+            )
 
     async def chat_message(self, event):
-        await self.send(text_data=json.dumps({
-            'message': event['message']
-        }))
+        await self.send(text_data=json.dumps(
+            {
+                'message': event['message']
+            }
+        ))
+
+    async def error(self, event):
+        await self.send(text_data=json.dumps(
+            {
+                'error': event['error']
+            }
+        ))
--- src/packs/middleware.py
+++ src/packs/middleware.py
@@ -1,4 +1,5 @@
 from channels.middleware import BaseMiddleware
+from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.tokens import UntypedToken
 from jwt import decode as jwt_decode
@@ -16,12 +17,20 @@ class JwtAuthMiddleware(BaseMiddleware):
             (key.decode("utf-8"), value.decode("utf-8"))
             for key, value in scope['headers']
         )
-        token = headers['authorization']
+
+        try:
+            token = headers['authorization']
+        except KeyError:
+            scope["user"] = AnonymousUser()
+
+            return await super().__call__(scope, *args, **kwargs)
 
         try:
             UntypedToken(token)
         except (InvalidToken, TokenError) as e:
-            return None
+            scope["user"] = AnonymousUser()
+
+            return await super().__call__(scope, *args, **kwargs)
 
         decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=['HS512'])
         scope["user"] = await get_user(validated_token=decoded_data)
