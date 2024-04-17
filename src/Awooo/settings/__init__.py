5366e0382a08244737719d08b36eafc344cd1883
Vitaliy Koltsov <vitaliy.koltsov.2002@gmail.com>
2024-04-17 15:38:08 +0300

    added backup for database

--- src/Awooo/settings/__init__.py
+++ src/Awooo/settings/__init__.py
@@ -7,7 +7,9 @@ load_dotenv()
 if os.getenv("LEVEL") == "PROD":
     print('RUN PROD MODE')
     from .prod import *
-
+elif os.getenv("LEVEL") == "LOCAL_PROD":
+    print('RUN LOCAL PROD MODE')
+    from .local_prod import *
 else:
     print('RUN LOCAL MODE')
     from .local import *
---
+++ src/Awooo/settings/local_prod.py
@@ -0,0 +1,46 @@
+import os
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent.parent
+load_dotenv()
+
+STATIC_ROOT = '/var/www/static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/media'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('HOST'),
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('redis', 6379)],
+        },
+    },
+}
